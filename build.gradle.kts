45a7a14dce35eada2876997132270bca34e858fa
MasterOfStar <21274896+MasterOfStar@users.noreply.github.com>
2026-04-15 03:13:55 +0800

    fix: 移动 pluginManagement 和 dependencyResolutionManagement 到 settings.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,22 +4,3 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
 }
-
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.name = "MiPayGPayLSP"
-include(":app")
